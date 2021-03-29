8930ce5a465e2804ba73956f3a6e4e94dd56457a
faisalaldajeh <learn.faisal@gmail.com>
2021-03-29 10:19:35 +0300

    adding h1 title

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,6 +4,7 @@ import  Directory  from "../../components/directory/directory.component";
 
 const HomePage = (item) => (
     <div className = 'homepage'>
+        <h1>Test Version</h1>
         <Directory/>
     </div>
         
